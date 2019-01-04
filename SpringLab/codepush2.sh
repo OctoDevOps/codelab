61b950866d603bcdd061855b16b8272a2ddfde62
Dinesh Ganesan <dinesh.ganesan@protonmail.com>
2019-01-04 12:59:11 -0500

    Updated jenkinsfile

--- SpringLab/codepush2.sh
+++ SpringLab/codepush2.sh
@@ -1,3 +1,4 @@
 git add .
 git commit -m "IAE-2 Updated patch Version"
 git push
+
---
+++ SpringLab/src/test/java/jptAutomationTests.java
@@ -0,0 +1,42 @@
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.actuate.autoconfigure.cloudfoundry.HealthEndpointCloudFoundryExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes={jptAutomation.class})
+//@ContextConfiguration(classes={jptAutomation.class})
+public class jptAutomationTests {
+	
+	@Autowired
+	private jptAutomation  m_objAutomation;
+
+	@Test
+	public void test_getPatchVersion(){
+		assertEquals("Validated getPatch() API","08",m_objAutomation.getPatchVersion());
+	}
+
+	@Test
+	public void test_getBuildVersion(){
+		assertEquals("Validated getBuildVersion() API","05",m_objAutomation.getBuildVersion());
+	}
+
+}
+
+
+/// Step #2. 
+/// Step #3. 
+// mvn test sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.login=1805353f12ac75d907948da15a5e3139c17d2dd9
+/*
+mvn sonar:sonar \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=1805353f12ac75d907948da15a5e3139c17d2dd9
+  */
